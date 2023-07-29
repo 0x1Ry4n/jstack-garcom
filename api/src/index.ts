4982934a4a936dac95134aebe340aee77b56cf78
Ryan <ryanlucas2018gz@gmail.com>
2023-07-29 18:12:46 -0300

    Api deploy

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,22 +1,27 @@
-import express from 'express';
-import path from 'node:path';
-import cors from 'cors';
+import express from "express";
+import path from "node:path";
+import cors from "cors";
 import http from "node:http";
+import * as dotenv from "dotenv";
 import { Server } from "socket.io";
-import mongoose from 'mongoose';
-import { router } from './router';
+import mongoose from "mongoose";
+import { router } from "./router";
+
+const envPath = path.resolve(__dirname, "..", ".env");
+dotenv.config({ path: envPath });
 
 const app = express();
 const server = http.createServer(app);
-
 export const io = new Server(server);
 
-mongoose.connect('mongodb://localhost:27017/garcom')
+mongoose
+  .connect(`${process.env.MONGODB_URI}`)
   .then(() => {
     const port = 3000;
 
-    app.use('/uploads', express.static(
-      path.resolve(__dirname, '..', 'uploads'))
+    app.use(
+      "/uploads",
+      express.static(path.resolve(__dirname, "..", "uploads"))
     );
 
     app.use(express.json());
@@ -25,9 +30,6 @@ mongoose.connect('mongodb://localhost:27017/garcom')
 
     server.listen(port, () => {
       console.log(`Rodando API-Garcom na porta ${port}`);
-    })
+    });
   })
-  .catch(() => console.warn('erro ao conectar no mongodb'));
-
-
-
+  .catch((e) => console.warn(`Erro ao conectar no MONGODB: ${e}`));
